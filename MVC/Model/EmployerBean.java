de4228009585ebb1ed19e2a4853d4349a692dbca
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2018-11-26 14:01:51 -0500

    Update EmployerBean.java

--- MVC/Model/EmployerBean.java
+++ MVC/Model/EmployerBean.java
@@ -7,6 +7,8 @@ public class EmployerBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private int employerId;
+	private String username;
+	private String password;
 	private String eName;
 	private String address;
 	private String contact;
@@ -22,6 +24,22 @@ public class EmployerBean implements Serializable{
 
 	public void setEmployerId(int employerId) {
 		this.employerId = employerId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getEName() {
