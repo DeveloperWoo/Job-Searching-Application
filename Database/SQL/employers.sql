7f50a890aa12d503625030e60694aa76ef5ac248
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2018-11-26 13:55:53 -0500

    Update employers.sql
    added two attributes, name and password

--- Database/SQL/employers.sql
+++ Database/SQL/employers.sql
@@ -2,6 +2,8 @@ use jobsearch;
 
 create table employers(
 employerID int not null auto_increment,
+username varchar(10) not null,
+password varchar(10) not null,
 eName varchar(50) not null,
 address varchar(200) not null,
 contact varchar(20) not null,
@@ -9,5 +11,3 @@ aboutUs varchar(2000) not null,
 website varchar(100) not null,
 primary key(employerID)
 );
-
-select * from employers;
